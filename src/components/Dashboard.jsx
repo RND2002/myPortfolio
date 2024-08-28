2ef5d6ec3dfc3d57ec4efcb6d66be70942ba15b1
Aryan Dwivedi <aryan.735921@gmail.comm>
2024-08-28 12:21:00 +0530

    new res

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,57 +1,67 @@
-import React from 'react'
-import SkillCard from './SkillCard'
-import Navbar from './Navbar'
-import About from './About'
-import Experience from './Experience'
-import Contact from './Contact'
-import code from '../assets/code.jpg'
-import designer from '../assets/bgmmpremium.png'
-import Work from './Work'
+import React from "react";
+import SkillCard from "./SkillCard";
+import Navbar from "./Navbar";
+import About from "./About";
+import Experience from "./Experience";
+import Contact from "./Contact";
+
+import designer from "../assets/bgmmpremium.png";
+import Work from "./Work";
 function Dashboard() {
   return (
     <>
-    <Navbar/>
-      <div className=' bg-background'>
-      {/* <Navbar/>
+      <Navbar />
+      <div className=" bg-background">
+        {/* <Navbar/>
       <About/>
       <div><center className='text-5xl mb-28 text-whitenew'>My Skills</center></div>
         <SkillCard/>
         <Experience/> */}
 
-<div className='h-96 md:mx-32'>
-        <div className='flex mx-12 mt-48 justify-between'>
-           
-      <div class="flex flex-col sm:flex-row items-center">
-  <div class="sm:w-1/2">
-    
-  <div class="flex items-center">
-  <h1 class="font-semibold text-3xl text-whitenew mr-4">I'am</h1>
-  <h2 class="text-5xl text-yellow">Aryan Dwivedi</h2>
-</div>
+        <div className="h-96 md:mx-32">
+          <div className="flex mx-12 mt-48 justify-between">
+            <div className="flex flex-col sm:flex-row items-center">
+              <div className="sm:w-1/2">
+                <div className="flex items-center">
+                  <h1 className="font-semibold text-3xl text-whitenew mr-4">
+                    I'am
+                  </h1>
+                  <h2 className="text-5xl text-yellow">Aryan Dwivedi</h2>
+                </div>
 
-    
-    
-    <h3 class=" text-3xl text-white font-bold">Software Engineer</h3>
-    <div class="mt-5">
-     <a  href='#contact'> <button class="border-4 border-yellow rounded-lg  text-whitenew text-xl font-bold px-8 py-2 bg-navColor">Contact Me</button></a>
-    </div>
-  </div>
-  <div class="sm:w-1/2 lg:w-1/2 ">
-    <img class="hidden lg:block object-cover h-60 w-96 rounded-lg mr-48" src={designer} alt=""/>
-  </div>
-</div>
-
-
-    </div>
-    </div>
-    <About/>
-    <SkillCard />
-    <Work/>
-    <Experience/>
-        <Contact/>
-        </div>  
+                <h3 className=" text-3xl text-white font-bold">
+                  Software Engineer
+                </h3>
+                <div className="mt-5">
+                  <a href="#contact">
+                    {" "}
+                    <button className="border-4 border-yellow rounded-lg  text-whitenew text-xl font-bold px-8 py-2 bg-navColor">
+                      Contact Me
+                    </button>
+                  </a>
+                </div>
+              </div>
+              <div className="sm:w-1/2 lg:w-1/2 ">
+                <img
+                  className="hidden lg:block object-cover h-60 w-96 rounded-lg mr-48"
+                  src={designer}
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <About />
+        <SkillCard />
+        <Work />
+        <Experience />
+        <Contact />
+      </div>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
+
+
+
